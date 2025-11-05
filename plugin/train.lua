4861e7f4d7b023761bb14cf80aec93ccc73c6f83
k.pradeep <hellopradeep69@gmail.com>
2025-11-05 23:29:45 +0530

    pls work 2

--- plugin/train.lua
+++ plugin/train.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("Train", function()
-	require("Train").train() -- assuming train is defined in lua/Train/init.lua
+	require("train").train() -- lowercase t matches lua/train/init.lua
 end, {
 	desc = "Run current file in tmux using Train plugin",
 })
